2fb1f04ef10c0ef1daa8f977b8b91fcf0bc51076
osparking <jbpark03@gmail.com>
2023-12-11 12:53:56 +0900

    EasyMock 테스트 메소드 상응 getContent 메소드 수정

--- src/main/java/ch08/jb/web/WebClient2.java
+++ src/main/java/ch08/jb/web/WebClient2.java
@@ -1,20 +1,20 @@
 package ch08.jb.web;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.Charset;
 
 public class WebClient2 {
 	public String getContent(ConnectionFactory connection) {
 		String workingContent = null;
 		StringBuffer content = new StringBuffer();
-		
-		try(InputStream is = connection.getData()) {
-			byte[] bytes = new byte[is.available()];
-			is.read(bytes);
-			workingContent = new String(bytes, Charset.forName("UTF8"));
-		} catch (IOException e) {
+
+		try (InputStream is = connection.getData()) {
+			int nextByte;
+			while (-1 != (nextByte = is.read())) {
+				content.append(String.valueOf((char) nextByte));
+			}
+			workingContent = content.toString();
 		} catch (Exception e) {
+			workingContent = null;
 		}
 		return workingContent;
 	}
